774146160649cdaddd72403e9fa643987f11f0ec
Asma22334585 <guesmihadil0@gmail.com>
2024-06-25 12:28:30 +0000

    my commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+	  // assigned a value but never used
     const task = true;
     const task2 = false;
   }
